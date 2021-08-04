cecff9710026b1e4fc85c7357fde53559f98a73e
HYUNAHSHIM <hyunahshim@naver.com>
2021-08-05 00:32:06 +0900

    commit

--- src/app.js
+++ src/app.js
@@ -1,70 +1,75 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const port = 8080;
 require('dotenv').config()
 
+app.use(cors());
+app.use(express.json());
+
 app.post(`/auth`, async (req, res) => {
-  const { code } = req.body;
-  const response = await axios.post('https://github.com/login/oauth/access_token', {
-    code, 
-    client_id: process.env.CLIENT_ID , 
-    client_secret: process.env.CLIENT_SECRET
-  },
-  {
-    headers: {
-      accept: 'application/json',
-    }
-  });
-
-  const token = response.data.access_token;
-  const result = [];
-
-  // 사용자 정보 가져오기
-  const user = await axios.get('https://api.github.com/user', {
-    headers: {
-      Authorization: `token ${token}`
-    }
-  });
-
-  // 사용자 레포 정보 가져오기
-  const repos = await axios.get(`https://api.github.com/users/${user.data.login}/repos`, {
-    headers: {
-      Authorization: `token ${token}`
-    }
-  });
-
-  // 사용자 레포별 언어 & 리드미 정보 가져오기
-  for await (let repo of repos.data) {
-    const languageData = await axios.get(`https://api.github.com/repos/${user.data.login}/${repo.data.name}/languages`, {
+  try {
+    const { code } = req.body;
+    const response = await axios.post('https://github.com/login/oauth/access_token', {
+      code, 
+      client_id: process.env.CLIENT_ID , 
+      client_secret: process.env.CLIENT_SECRET
+    },
+    {
       headers: {
-        Authorization: `token ${token}`
+        accept: 'application/json',
       }
     });
-    const readmeData = await axios.get(`https://api.github.com/repos/${user.data.login}/${repo.data.name}/contents/README.md`, {
+
+    const token = response.data.access_token;
+    const result = [];
+
+    // 사용자 정보 가져오기
+    const user = await axios.get('https://api.github.com/user', {
       headers: {
         Authorization: `token ${token}`
       }
     });
 
-    let tmp = {};
-    tmp.name = repo.name;
-    tmp.owner = repo.owner;
-    tmp.description = repo.description;
-    tmp.url = repo.html_url;
-    tmp.languages = languageData.data;
-    tmp.readme = readmeData.data;
-    tmp.created_at = repo.created_at;
-    tmp.updated_at = repo.updated_at;
+    // 사용자 레포 정보 가져오기
+    const repos = await axios.get(`https://api.github.com/users/${user.data.login}/repos`, {
+      headers: {
+        Authorization: `token ${token}`
+      }
+    });
 
-    result.push(tmp);
+    // 사용자 레포별 언어 & 리드미 정보 가져오기
+    for await (let repo of repos.data) {
+      const languageData = await axios.get(`https://api.github.com/repos/${user.data.login}/${repo.name}/languages`, {
+        headers: {
+          Authorization: `token ${token}`
+        }
+      });
+      const readmeData = await axios.get(`https://raw.githubusercontent.com/${user.data.login}/${repo.name}/${repo.default_branch}/README.md`);
+
+      let tmp = {};
+      tmp.name = repo.name;
+      tmp.owner = repo.owner;
+      tmp.description = repo.description;
+      tmp.url = repo.html_url;
+      tmp.languages = languageData.data;
+      tmp.readme = readmeData.data;
+      tmp.created_at = repo.created_at;
+      tmp.updated_at = repo.updated_at;
+
+      result.push(tmp);
+    }
+
+    // 응답
+    res.status(200).json({
+      user: user.data.login,
+      repos: result
+    });
+  } catch (err) {
+    console.error(err.message);
   }
-
-  // 응답
-  res.status(200).json({
-    user: user.data.login,
-    repos: result
-  });
+  
 });
 
 app.use((req, res, next) => {
